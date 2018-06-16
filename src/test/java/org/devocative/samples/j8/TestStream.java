70809cf8cb7062c6e76ab5e7de83179b65b17efd
Mehdi Bizhani <mbizhani@gmail.com>
2018-06-16 23:20:50 +0430

    complete TestStream learning

--- src/test/java/org/devocative/samples/j8/TestStream.java
+++ src/test/java/org/devocative/samples/j8/TestStream.java
@@ -8,8 +8,11 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -31,6 +34,7 @@ public class TestStream {
 		List<Integer> list = iterate
 			.limit(5)
 			.map(n -> n[1])
+			//.collect(ArrayList::new, ArrayList::add, ArrayList::addAll)
 			.collect(Collectors.toList());
 		assertEquals(list, Arrays.asList(1, 2, 3, 5, 8));
 	}
@@ -46,65 +50,141 @@ public class TestStream {
 		final List<String> distinctWords =
 			Arrays.asList("test01", "passed", "test02", "test11", "failed");
 
-		final List<String> sortedWords =
+		final List<String> sortedDistinctWords =
 			Arrays.asList("test11", "test02", "test01", "passed", "failed");
 
 		final Path file = Files.createTempFile("__", "__");
 		Files.write(file, content.getBytes());
 
 		// Grepping lines containing '1'
-		try (Stream<String> stream = Files.lines(file)) {
-			String result = stream
+		try (Stream<String> lines = Files.lines(file)) {
+			String result = lines
 				.filter(line -> line.contains("1"))
 				.collect(Collectors.joining("\n"));
 			assertEquals(grepped, result);
 		}
 
 		// List of words
-		try (Stream<String> stream = Files.lines(file)) {
-			List<String> result = stream
+		try (Stream<String> lines = Files.lines(file)) {
+			List<String> result = lines
 				.flatMap(line -> Stream.of(line.split("\\s")))
 				.collect(Collectors.toList());
 			assertEquals(words, result);
 		}
 
 		// List of distinct words
-		try (Stream<String> stream = Files.lines(file)) {
-			List<String> result = stream
+		try (Stream<String> lines = Files.lines(file)) {
+			List<String> result = lines
 				.flatMap(line -> Stream.of(line.split("\\s")))
 				.distinct()
 				.collect(Collectors.toList());
 			assertEquals(distinctWords, result);
 		}
 
-		// List of distinct & descending sorted words
-		try (Stream<String> stream = Files.lines(file)) {
-			List<String> result = stream
+		// List of distinct & descending-sorted words
+		try (Stream<String> lines = Files.lines(file)) {
+			List<String> result = lines
 				.flatMap(line -> Stream.of(line.split("\\s")))
 				.distinct()
 				.sorted(Comparator.reverseOrder())
 				.collect(Collectors.toList());
-			assertEquals(sortedWords, result);
+			assertEquals(sortedDistinctWords, result);
+		}
+
+		// List of distinct & descending-sorted words
+		try (Stream<String> lines = Files.lines(file)) {
+			String result = lines
+				.flatMap(line -> Stream.of(line.split("\\s")))
+				.distinct()
+				.sorted(Comparator.reverseOrder())
+				.findFirst() // min(Comparator.reverseOrder()) instead of sorted() & findFirst()
+				.get();
+			assertEquals("test11", result);
 		}
 
 		// Count number of words
-		try (Stream<String> stream = Files.lines(file)) {
-			long result = stream
+		try (Stream<String> lines = Files.lines(file)) {
+			long result = lines
 				.flatMap(line -> Stream.of(line.split("\\s")))
 				.count();
 			assertEquals(words.size(), result);
 		}
 
-		// Count number of characters of words
-		try (Stream<String> stream = Files.lines(file)) {
-			long result = stream
-				.flatMap(line -> Stream.of(line.split("\\s")))
-				.mapToLong(String::length)
-				.sum();
-			assertEquals(36, result);
+		// Count number of characters of words (1/2)
+		String fileAsStr = new String(Files.readAllBytes(file));
+		long result = Pattern.compile("\\s")
+			.splitAsStream(fileAsStr)
+			.mapToLong(String::length)
+			.sum();
+		assertEquals(36, result);
+
+		// Count number of characters of words (2/2)
+		fileAsStr = new String(Files.readAllBytes(file));
+		result = Pattern.compile("\\s")
+			.splitAsStream(fileAsStr)
+			.reduce(0L,
+				(total, word) -> total + word.length(),
+				(total1, total2) -> total1 + total2);
+		assertEquals(36, result);
+	}
+
+	@Test
+	public void testFactorial() {
+		long result = LongStream
+			//.range(1, 5)        [1, 5)
+			.rangeClosed(1, 5) // [1, 5]
+			.reduce((left, right) -> left * right)
+			.getAsLong();
+
+		assertEquals(120, result);
+
+		result = LongStream
+			//.range(1, 5)        [1, 5)
+			.rangeClosed(1, 5) // [1, 5]
+			.reduce(1, (left, right) -> left * right);
+
+		assertEquals(120, result);
+	}
+
+	@Test
+	public void testCollectors() {
+		List<Employee> list = Arrays.asList(
+			new Employee("John", 5000),
+			new Employee("Jack", 6000),
+			new Employee("Jack", 7000),
+			new Employee("Bill", 3000));
+
+		Map<String, Employee> map = list.stream()
+			.collect(Collectors.toMap(Employee::getName, Function.identity(), (curV, newV) -> newV));
+
+		assertEquals(3, map.size());
+		assertEquals(7000, map.get("Jack").getSalary().intValue());
+
+
+	}
+
+	// ------------------------------
+
+	class Employee {
+		private String name;
+		private Integer salary;
+
+		Employee(String name, Integer salary) {
+			this.name = name;
+			this.salary = salary;
 		}
 
-		IntStream.of(1, 2, 3);
+		String getName() {
+			return name;
+		}
 
+		Integer getSalary() {
+			return salary;
+		}
+
+		@Override
+		public String toString() {
+			return getName() + ", " + getSalary();
+		}
 	}
 }
